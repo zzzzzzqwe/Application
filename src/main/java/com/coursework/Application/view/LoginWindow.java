aabd8d3564d13ceec8e09bdbe7cdbb76757d9c45
zzzzzzqwe <dima.gachayev@gmail.com>
2025-06-04 17:27:35 +0300

    fix: LoginWindow size adjustments

--- src/main/java/com/coursework/Application/view/LoginWindow.java
+++ src/main/java/com/coursework/Application/view/LoginWindow.java
@@ -53,7 +53,7 @@ public class LoginWindow {
             }
         });
 
-        stage.setScene(new Scene(layout));
+        stage.setScene(new Scene(layout, 150, 180));
         stage.showAndWait();
 
         return UserSession.getUsername() != null;
